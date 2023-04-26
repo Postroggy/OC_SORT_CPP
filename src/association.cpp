7e10ca8ce581f76b9c6b65f85ee1c01b16207918
postroggy <2451527090@qq.com>
2023-04-26 22:30:11 +0800

    数据关联部分的 iou_batch测试通过

--- src/association.cpp
+++ src/association.cpp
@@ -1,6 +1,6 @@
 #include "../include/association.h"
-namespace ocsort{
-    std::tuple<Eigen::MatrixXd,Eigen::MatrixXd> speed_direction_batch(const Eigen::MatrixXd &dets,
+namespace ocsort {
+    std::tuple<Eigen::MatrixXd, Eigen::MatrixXd> speed_direction_batch(const Eigen::MatrixXd &dets,
                                                                        const Eigen::MatrixXd &tracks) {
         Eigen::VectorXd CX1 = (dets.col(0) + dets.col(2)) / 2.0;
         Eigen::VectorXd CY1 = (dets.col(1) + dets.col(3)) / 2.f;
@@ -11,15 +11,56 @@ namespace ocsort{
         Eigen::MatrixXd norm = (dx.array().square() + dy.array().square()).sqrt() + 1e-6f;
         dx = dx.array() / norm.array();
         dy = dy.array() / norm.array();
-        return std::make_tuple(dy,dx);
+        return std::make_tuple(dy, dx);
     }
+    Eigen::MatrixXd iou_batch(const Eigen::MatrixXd &bboxes1, const Eigen::MatrixXd &bboxes2) {
+        Eigen::Matrix<double, Eigen::Dynamic, 1> a = bboxes1.col(0);// bboxes1[..., 0] (2,1)
+        Eigen::Matrix<double, 1, Eigen::Dynamic> b = bboxes2.col(0);// bboxes2[..., 0] (2,1) 需要转成(1,2)
+        Eigen::MatrixXd xx1 = (a.replicate(1, a.rows())).cwiseMax(b.replicate(b.cols(), 1));
+        a = bboxes1.col(1);
+        b = bboxes2.col(1);
+        Eigen::MatrixXd yy1 = (a.replicate(1, a.rows())).cwiseMax(b.replicate(b.cols(), 1));
+        a = bboxes1.col(2);
+        b = bboxes2.col(2);
+        Eigen::MatrixXd xx2 = (a.replicate(1, a.rows())).cwiseMin(b.replicate(b.cols(), 1));
+        a = bboxes1.col(3);
+        b = bboxes2.col(3);
+        Eigen::MatrixXd yy2 = (a.replicate(1, a.rows())).cwiseMin(b.replicate(b.cols(), 1));
+        Eigen::MatrixXd w = (xx2 - xx1).cwiseMax(0);
+        Eigen::MatrixXd h = (yy2 - yy1).cwiseMax(0);
+        Eigen::MatrixXd wh = w.array() * h.array();
+        //    Eigen::MatrixXd part1= (bboxes1.col(2) - bboxes1.col(0)).array()*(bboxes1.col(3) - bboxes1.col(1)).array();
+        a = (bboxes1.col(2) - bboxes1.col(0)).array() * (bboxes1.col(3) - bboxes1.col(1)).array();
+        //    Eigen::Matrix<double,1,Eigen::Dynamic> part2= (bboxes2.col(2) - bboxes2.col(0)).array()*(bboxes2.col(3) - bboxes2.col(1)).array();
+        b = (bboxes2.col(2) - bboxes2.col(0)).array() * (bboxes2.col(3) - bboxes2.col(1)).array();
+        // 做加法，但是还需要广播一下
+        Eigen::MatrixXd part1_ = a.replicate(1, a.rows());
+        Eigen::MatrixXd part2_ = b.replicate(b.cols(), 1);
+        Eigen::MatrixXd Sum = part1_ + part2_ - wh;
+        return wh.cwiseQuotient(Sum);
+    }
+
     std::tuple<Eigen::MatrixXd, Eigen::MatrixXd, Eigen::MatrixXd> ocsort::associate(Eigen::MatrixXd detections, Eigen::MatrixXd trackers, float iou_threshold, Eigen::MatrixXd velocities, Eigen::MatrixXd previous_obs, float vdc_weight) {
-        if(trackers.rows() == 0 ) return std::make_tuple(Eigen::VectorXd::Zero(0,2),Eigen::VectorXd::LinSpaced(detections.rows(), 0, detections.rows()-1),Eigen::VectorXd::Zero(0,5));
+        if (trackers.rows() == 0) return std::make_tuple(Eigen::VectorXd::Zero(0, 2), Eigen::VectorXd::LinSpaced(detections.rows(), 0, detections.rows() - 1), Eigen::VectorXd::Zero(0, 5));
 
         Eigen::MatrixXd Y, X;
         auto result = speed_direction_batch(detections, previous_obs);
         Y = std::get<0>(result);
         X = std::get<1>(result);
-
+        Eigen::MatrixXd inertia_Y = velocities.col(0);
+        Eigen::MatrixXd inertia_X = velocities.col(1);
+        // todo 这里maybe会有问题
+        inertia_Y = inertia_Y.transpose().replicate(1, inertia_Y.cols());
+        inertia_X = inertia_X.transpose().replicate(1, inertia_X.cols());
+        // 然而这里和numpy不一样，eigen中需要显式声明用 逐元素点乘
+        Eigen::MatrixXd diff_angle_cos = inertia_X.array() * X.array() + inertia_Y.array() * Y.array();
+        // 计算了两个向量的余弦后，应该将他们的值限制在 -1 ~ 1 之间，保证计算的正确性
+        diff_angle_cos = (diff_angle_cos.array().min(1).max(-1)).matrix();
+        // 计算反余弦值
+        Eigen::MatrixXd diff_angle = Eigen::acos(diff_angle_cos.array());
+        // fixme？这个掩码是拿来干嘛的？
+        Eigen::Array<bool, 1, Eigen::Dynamic> valid_mask = Eigen::Array<bool, Eigen::Dynamic, 1>::Ones(previous_obs.rows());
+        valid_mask.colwise() *= (previous_obs.col(4).array() >= 0);// 然后numpy中，这里应该是个行向量的，但是C++中却变成了列向量
+        Eigen::MatrixXd iou_matrix = iou_batch(detections, trackers);
     }
-}
+}// namespace ocsort
--- test.cpp
+++ test.cpp
@@ -2,6 +2,11 @@
 #include "iostream"
 using namespace std;
 using namespace Eigen;
+#include <Eigen/Core>
+
+#include <Eigen/Core>
+
+
 
 int main(int argc, char *argv[]) {
     Eigen::MatrixXd dets(2, 4), tracks(2, 4);
@@ -9,4 +14,34 @@ int main(int argc, char *argv[]) {
             6, 2, 2, 2;
     tracks << 1, 6, 5, 7,
             2, 8, 1, 5;
+    MatrixXd inertia_X(2, 2);
+    MatrixXd inertia_Y(2, 2);
+    MatrixXd X(2, 2);
+    MatrixXd Y(2, 2);
+    X << 0.59999976, 0.21693041, 0.83205006, 0.48564284;
+    Y << -0.79999968, -0.97618685, -0.55470004, -0.87415711;
+    inertia_X << -0.44721346, -0.44721346, 0.97014203, 0.97014203;
+    inertia_Y << -0.89442692, -0.89442692, -0.24253551, -0.24253551;
+    MatrixXd diff_angle_cos(2, 2);
+    diff_angle_cos << 0.44721328, 0.7761136, 0.9417412, 0.68315667;
+    MatrixXd diff_angle = Eigen::acos(diff_angle_cos.array());
+
+
+    Eigen::MatrixXd previous_obs(5, 5);
+    previous_obs << 1, 2, 3, 4, -1,
+            5, 6, 7, -1, -2,
+            9, 10, 11, 12, 13,
+            14, -3, 15, 16, 17,
+            18, 19, 20, -4, 21;
+    Eigen::Array<bool, Eigen::Dynamic, 1> valid_mask = Eigen::Array<bool, Eigen::Dynamic, 1>::Ones(previous_obs.rows());
+    valid_mask.colwise() *= (previous_obs.col(4).array() >= 0);
+
+    Eigen::MatrixXd a(2, 6);
+    Eigen::MatrixXd b(2, 5);
+    a << 0., 145., 55., 454., 0.66, 0.,
+            849., 205., 924., 364., 0.63, 0.;
+    b << 851., 203., 923., 364., 0.,
+            0., 153., 51., 451., 0.;
+    cout<<    iou_batch(a, b);
+
 }
